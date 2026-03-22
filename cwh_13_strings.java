6f2f2f18e5fe4e7fab9f58362584bddbbb4e6280
Prateektrivedi22 <prateektrivedi300@gmail.com>
2026-03-22 23:27:12 +0530

    Add files via upload

---
+++ cwh_13_strings.java
@@ -0,0 +1,19 @@
+//  import java.util.Scanner;
+ public class cwh_13_strings {
+   
+public static void main(String[] args) {
+    // String name = "prati";
+    // System.out.print("the name is : ");
+    // // System.out.println(name);
+    // int a = 6 ;
+    // float b = 6.066f;
+    // System.out.printf("the value of a is %d and the value b is %8.2f" , a , b );
+     Scanner sc = new Scanner(System.in);
+     String st = sc.next();
+     String st = sc.nextLine();
+
+
+
+    
+}
+}
