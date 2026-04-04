3352437b58ea7951275d8898b3e23dbe30686b48
Prateektrivedi22 <prateektrivedi300@gmail.com>
2026-04-04 23:51:45 +0530

    Add files via upload

---
+++ cwh_16_conditionals.java
@@ -0,0 +1,13 @@
+public class cwh_16_conditionals {
+     public static void main (String[] args){
+// here = refers to assign the value i.e.assignment operator
+int age = 50;
+// here == refers to check the value i.e. equality operator
+if(age == 18){
+    System.out.println("yes boy you can drive.");
+}
+    else{
+        System.out.println("no boy you cant drive");
+}
+    }
+}
---
+++ cwh_17_logicals.java
@@ -0,0 +1,40 @@
+public class cwh_17_logicals {
+    public static void main(String[] args) {
+        // LOGICAL OPERATORS IN JAVA 
+        // 1. logical AND (&&) OPERATOR
+        // 2. LOGICAL OR (||) OPERATOR
+        // 3. LOGICAl NOT (!) OPERTOR
+        // System.out.println(" FOR LOGICAL AND ...");
+        // boolean a = true;
+        // boolean b = true;
+        // boolean c = true;
+
+        // if (a &&b &&c) {
+        //     System.out.println("yay i am learning java ");
+        // }
+        //     else { 
+        //         System.out.println(( "i am not learning java "));
+
+        //     }
+            //   System.out.println(" FOR LOGICAL OR ...");
+        boolean a = false;
+        boolean b = false;
+        
+
+        // if (a ||b) {
+        //     System.out.println("yay i am learning java ");
+        // }
+        //     else { 
+        //         System.out.println(( "i am not learning java "));
+
+        //     }
+                System.out.println(" FOR LOGICAL NOT ...");
+                System.out.print(("not(a) is "));
+                System.out.println((!a));
+                 System.out.print(("not(b) is "));
+                System.out.println((!b));
+
+            
+        }
+    }
+
