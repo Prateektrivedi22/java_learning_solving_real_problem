f318252f50969853212d1ad5c34129a86ae0e743
Prateektrivedi22 <prateektrivedi300@gmail.com>
2026-04-07 23:50:14 +0530

    Add files via upload

---
+++ cwh_20_ex2.java
@@ -0,0 +1,25 @@
+// write a java prgrm to print stone rock scissor game 
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class cwh_20_ex2{
+    public static void main(String[] args) {
+        System.out.println( "1 for rock, 2 for paper , 3 for scissors" );
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the nos from 1 to 3");
+        int user = sc.nextInt();
+        System.out.println("your number is : " + user);
+        Random rand = new Random();
+        int computer = rand.nextInt(3) + 1;
+        System.out.println("computer no is : " + computer );
+
+        if("user choice == computer choice" != null );
+        System.out.println("match draw");
+        else if(user choice == 1 &&){
+
+
+        }
+
+    }
+}
