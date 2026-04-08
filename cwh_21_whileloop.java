82e2e59ef73da8e47fbea309902af9ab00284016
Prateektrivedi22 <prateektrivedi300@gmail.com>
2026-04-08 08:56:15 +0530

    Add files via upload

---
+++ cwh_21_whileloop.java
@@ -0,0 +1,12 @@
+public class cwh_21_whileloop {
+    public static void main(String[] args) {
+        System.out.println("using while loop ");
+        int i = 1;
+        while (i<=3){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("finish running while loop");
+    }
+    
+}
