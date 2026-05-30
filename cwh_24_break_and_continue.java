e442c3fd43a2fee22e9bd2d3ae78110f9b3132fa
Prateektrivedi22 <prateektrivedi300@gmail.com>
2026-05-30 21:04:19 +0530

    Add files via upload

---
+++ cwh_24_break_and_continue.java
@@ -0,0 +1,46 @@
+public class cwh_24_break_and_continue {
+public static void main(String[]args) {
+// break and continue using loops
+// for(int i = 0 ; i<5; i++){
+//     System.out.println(i);
+//     System.out.println("ending the loop");
+//     if (i==2 ){
+//         System.out.println("loop ends here");
+//         break;
+//     }
+// int i = 0;
+
+// while(i<5){
+//     System.out.println(i);
+
+// if(i==2){
+//     System.out.println("loop ends");
+//     break;
+// }
+// i++;
+
+// using continue
+
+ for(int i = 0 ; i<5; i++){
+     
+     if (i==2 ){
+         System.out.println("loop ends here");
+         continue;}
+     System.out.println(i);
+     System.out.println("ending the loop");
+     
+     
+        
+}
+}
+
+
+}
+
+
+
+
+    
+
+    
+
