2c41826b0c59ada7c4666a9722bb53b19f6b56bc
Prateektrivedi22 <prateektrivedi300@gmail.com>
2026-06-08 07:02:44 +0530

    Add salary attribute and getter to employee class

--- cwh_38_creating_our_own_class_in_java.java
+++ cwh_38_creating_our_own_class_in_java.java
@@ -1,11 +1,15 @@
   class employee{
     int id;
+    int salary;
     String name;
 
 
     public void printdetails(){
 System.out.println("my id is :  " + id );
 System.out.println(" my name is : " + name );
+    } 
+    public int getsalary(){
+        return salary;
     } 
 
     
@@ -28,13 +32,16 @@ System.out.println("This is our custom class ");
 
      harry.id = 12;
       harry.name =  "harry";
+      harry.salary = 34;
+
 jhon.id = 21;
 jhon.name = "jhon tripathi";
-
+jhon.salary = 45;
       //printing attributtes
       harry.printdetails();
       jhon.printdetails();
-
+int salary = jhon.getsalary();
+System.out.println(salary);
       //System.out.println("The id of new employee is : " + harry.id);
      // System.out.println("the name of new employee is : " + harry.name);
 
