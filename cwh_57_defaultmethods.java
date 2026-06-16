78cb098ff1c9dd36b349348915f3ecca28b8465d
Prateektrivedi22 <prateektrivedi300@gmail.com>
2026-06-16 23:56:37 +0530

    Add files via upload

---
+++ cwh_57_defaultmethods.java
@@ -0,0 +1,18 @@
+interface camera{
+    void takesnap();
+    void recovervideo();
+
+
+
+}
+
+interface wifi {
+    String[] getnetworks();
+    void connecttonetwork( String network);
+
+}
+
+
+public class cwh_57_defaultmethods {
+    
+}
